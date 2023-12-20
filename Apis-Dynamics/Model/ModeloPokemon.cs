f8ccf8da846aca5c4df33cfcb946799fe8c62901
Regui <Regui@DESKTOP-2362DLV>
2023-12-20 18:18:48 -0300

    Adicionar arquivos de projeto.

---
+++ Apis-Dynamics/Model/ModeloPokemon.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Apis_Dynamics.Model;
+
+internal class ModeloPokemon
+{
+    [JsonPropertyName("name")]
+    public string Nome { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome do pokemon é: {Nome}");
+    }
+}
---
+++ Apis-Dynamics/Program.cs
@@ -0,0 +1,21 @@
+﻿using Apis_Dynamics.Model;
+using System.Text.Json;
+
+
+using (HttpClient client = new())
+{
+    try
+    {
+        Random random = new();
+        int idPokemon = random.Next(1, 1026);
+        var conexao = await client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{idPokemon}");
+        var nomeDoPokemon = JsonSerializer.Deserialize<ModeloPokemon>(conexao)!;
+
+        nomeDoPokemon.ExibirDetalhes();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+    }
+}
+
